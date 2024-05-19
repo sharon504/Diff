7708670a5bfe4d26849a3b4ad8294ccade3e9402
Athul Anoop <athulanoop2020@gmail.com>
2024-05-19 17:59:10 +0530

    Update user.js
    Altered the mongoose schema so that only important fields are required and added an email field specifically for authentication

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -7,12 +7,13 @@ export const fileSchema = new mongoose.Schema({
 });
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  name: { type: String, required: true },
+  email: { type: String, required: true },
   password: { type: String, required: true },
-  yearOfStudy: { type: Number, required: true },
-  techStacks: { type: Array, required: true },
-  projects: { type: Array, required: true },
+  name: { type: String, required: true },
+  username: { type: String, required: false },
+  yearOfStudy: { type: Number, required: false },
+  techStacks: { type: Array, required: false },
+  projects: { type: Array, required: false },
   joinedAt: { type: Date, default: Date.now },
   qualifications: {
     resume: fileSchema,
@@ -20,10 +21,10 @@ const userSchema = new mongoose.Schema({
     portfolio: { type: String, required: false },
   },
   contact: {
-    linkedin: { type: String, required: true },
-    github: { type: String, required: true },
-    phone: { type: String, required: true },
-    email: { type: String, required: true },
+    linkedin: { type: String, required: false },
+    github: { type: String, required: false },
+    phone: { type: String, required: false },
+    email: { type: String, required: false },
   },
 });
 
