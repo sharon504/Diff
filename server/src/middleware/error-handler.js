02382d86a553a04db80f2ff2f3027cdeddc2c88f
Athul Anoop <athulanoop2020@gmail.com>
2024-06-03 23:37:21 +0530

    Create error-handler.js
    Middleware to handle errors

---
+++ server/src/middleware/error-handler.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+class ErrorHandler extends Error {
+  constructor(code, message) {
+    super(message);
+    this.code = code;
+  }
+}
+
+const asyncErrorHandler = (fn) => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next);
+};
+
+const errorHandler = (err, req, res, next) => {
+  if (err instanceof ErrorHandler) {
+    res.status(err.code).json({ message: err.message });
+  } else if (err instanceof mongoose.Error.ValidationError) {
+    const messages = Object.values(err.errors).map((value) => value.message);
+    res.status(400).json({ message: messages });
+  } else {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+export default ErrorHandler;
+export { asyncErrorHandler, errorHandler };
