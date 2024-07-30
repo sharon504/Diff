156b4f18753e02a6d0f0e8e5e0a683af7f1fa81b
Athul Anoop <athulanoop2020@gmail.com>
2024-07-30 21:34:20 +0530

    updated formatting

--- client/src/components/projects/projects-gallery.tsx
+++ client/src/components/projects/projects-gallery.tsx
@@ -1,89 +1,89 @@
 import React, { useEffect, useState } from "react";
 import { useStore } from "@nanostores/react";
 import {
-  type T_project,
-  setProjects,
-  processedProjectsAtom,
+	type T_project,
+	setProjects,
+	processedProjectsAtom,
 } from "@store/projects-store";
 
 const ProjectCard = ({ project }: { project: T_project }) => {
-  const { _id, name, description, techStacks, status, projectURL } = project;
-  return (
-    <div className="h-96 p-3 flex flex-col justify-between gap-4 bg-white rounded-lg shadow-md overflow-hidden hover:scale-[1.02] ease-in-out duration-500">
-      <div className="h-full flex flex-col justify-between gap-4">
-        <div className="flex flex-col justify-between gap-4">
-          <div className="flex justify-between gap-2">
-            <h3 className="text-xl font-bold">{name}</h3>
-            <span
-              className={`h-max text-sm font-semibold px-4 py-1 rounded-full ${
-                status === "active"
-                  ? "bg-green-100 text-green-800"
-                  : status === "completed"
-                    ? "bg-blue-100 text-blue-800"
-                    : "bg-gray-100 text-gray-800"
-              }`}
-            >
-              {status}
-            </span>
-          </div>
-          <p className="max-h-48 overflow-y-hidden text-gray-700">
-            {description}
-          </p>
-        </div>
+	const { _id, name, description, techStacks, status, projectURL } = project;
+	return (
+		<div className="h-96 p-3 flex flex-col justify-between gap-4 bg-white rounded-lg shadow-md overflow-hidden hover:scale-[1.02] ease-in-out duration-500">
+			<div className="h-full flex flex-col justify-between gap-4">
+				<div className="flex flex-col justify-between gap-4">
+					<div className="flex justify-between gap-2">
+						<h3 className="text-xl font-bold">{name}</h3>
+						<span
+							className={`h-max text-sm font-semibold px-4 py-1 rounded-full ${
+								status === "active"
+									? "bg-green-100 text-green-800"
+									: status === "completed"
+										? "bg-blue-100 text-blue-800"
+										: "bg-gray-100 text-gray-800"
+							}`}
+						>
+							{status}
+						</span>
+					</div>
+					<p className="max-h-48 overflow-y-hidden text-gray-700">
+						{description}
+					</p>
+				</div>
 
-        <ul className="flex flex-wrap gap-2">
-          {techStacks.slice(0, 5).map((techstack: string) => (
-            <li
-              key={techstack}
-              className="min-w-12 px-2 py-1 text-center text-gray-500 rounded-full bg-gray-200"
-            >
-              {techstack}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="flex justify-between items-center">
-        <a
-          href={projectURL}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="flex gap-1 px-4 py-1 rounded-full border border-black hover:bg-neutral-900 hover:text-white ease-in-out duration-300 focus:outline-none"
-        >
-          GitHub
-        </a>{" "}
-        <a
-          href={`/projects/${_id}`}
-          className="flex px-4 py-1 rounded-full border border-blue-500 text-blue-500 hover:bg-blue-500 ease-in-out duration-300 hover:text-white focus:outline-none"
-        >
-          View
-        </a>
-        <button className="flex px-4 py-1 rounded-full border border-green-500 text-green-500 hover:bg-green-500 ease-in-out duration-300 hover:text-white focus:outline-none">
-          Apply
-        </button>
-      </div>
-    </div>
-  );
+				<ul className="flex flex-wrap gap-2">
+					{techStacks.slice(0, 5).map((techstack: string) => (
+						<li
+							key={techstack}
+							className="min-w-12 px-2 py-1 text-center text-gray-500 rounded-full bg-gray-200"
+						>
+							{techstack}
+						</li>
+					))}
+				</ul>
+			</div>
+			<div className="flex justify-between items-center">
+				<a
+					href={projectURL}
+					target="_blank"
+					rel="noopener noreferrer"
+					className="flex gap-1 px-4 py-1 rounded-full border border-black hover:bg-neutral-900 hover:text-white ease-in-out duration-300 focus:outline-none"
+				>
+					GitHub
+				</a>{" "}
+				<a
+					href={`/projects/${_id}`}
+					className="flex px-4 py-1 rounded-full border border-blue-500 text-blue-500 hover:bg-blue-500 ease-in-out duration-300 hover:text-white focus:outline-none"
+				>
+					View
+				</a>
+				<button className="flex px-4 py-1 rounded-full border border-green-500 text-green-500 hover:bg-green-500 ease-in-out duration-300 hover:text-white focus:outline-none">
+					Apply
+				</button>
+			</div>
+		</div>
+	);
 };
 const ProjectsGallery = () => {
-  const $processedProjects = useStore(processedProjectsAtom);
-  const fetchProjects = async () => {
-    const response = await fetch("http://localhost:2000/api/v1/projects");
-    const projects = await response.json();
-    setProjects(projects);
-  };
+	const $processedProjects = useStore(processedProjectsAtom);
+	const fetchProjects = async () => {
+		const response = await fetch("http://localhost:2000/api/v1/projects");
+		const projects = await response.json();
+		setProjects(projects);
+	};
 
-  useEffect(() => {
-    fetchProjects();
-  }, []);
-  return (
-    <ul className="grid gap-8 md:grid-cols-3">
-      {$processedProjects.map((project: T_project) => (
-        <li key={project._id}>
-          <ProjectCard project={project} />
-        </li>
-      ))}
-    </ul>
-  );
+	useEffect(() => {
+		fetchProjects();
+	}, []);
+	return (
+		<ul className="grid gap-8 md:grid-cols-3">
+			{$processedProjects.map((project: T_project) => (
+				<li key={project._id}>
+					<ProjectCard project={project} />
+				</li>
+			))}
+		</ul>
+	);
 };
 
 export default ProjectsGallery;
--- client/src/store/projects-store.ts
+++ client/src/store/projects-store.ts
@@ -1,149 +1,149 @@
 import { atom } from "nanostores";
 
 export type T_project = {
-  _id: string;
-  name: string;
-  description: string;
-  owner: string;
-  techStacks?: string[];
-  status?: "active" | "completed" | "archived";
-  projectURL?: string;
-  readmeFile?: string;
-  contributors?: string[];
-  sponsorship?: {
-    sponsor?: string;
-    amount?: number;
-    startDate?: Date;
-    endDate?: Date;
-  }[];
-  vacancies?: string[];
-  applications?: {
-    user?: string;
-    status?: "pending" | "accepted" | "rejected";
-  }[];
-  summary?: string;
-  createdAt: Date;
-  modifiedAt: Date;
+	_id: string;
+	name: string;
+	description: string;
+	owner: string;
+	tech_stacks?: string[];
+	status?: "active" | "completed" | "archived";
+	project_url?: string;
+	readme_file?: string;
+	contributors?: string[];
+	sponsorship?: {
+		sponsor?: string;
+		amount?: number;
+		startDate?: Date;
+		endDate?: Date;
+	}[];
+	vacancies?: string[];
+	applications?: {
+		user?: string;
+		status?: "pending" | "accepted" | "rejected";
+	}[];
+	summary?: string;
+	created_at: Date;
+	modified_at: Date;
 };
 
 export type T_sortOrder = "asc" | "desc";
 
 export type T_searchQuery = {
-  queryStr: string;
-  filters: {
-    techStacks?: string[];
-    status?: "active" | "completed" | "archived";
-  };
-  sortBy: string;
-  sortOrder: T_sortOrder;
+	queryStr: string;
+	filters: {
+		tech_stacks?: string[];
+		status?: "active" | "completed" | "archived";
+	};
+	sort_by: string;
+	sort_order: T_sortOrder;
 };
 
 export const projectsAtom = atom<T_project[]>([]);
 export const processedProjectsAtom = atom<T_project[]>([]);
 export const selectedProject = atom<string | null>(null);
 export const searchQueryAtom = atom<T_searchQuery>({
-  queryStr: "",
-  filters: {
-    techStacks: [],
-    status: undefined,
-  },
-  sortBy: "name",
-  sortOrder: "asc",
+	queryStr: "",
+	filters: {
+		tech_stacks: [],
+		status: undefined,
+	},
+	sort_by: "name",
+	sort_order: "asc",
 });
 export const availableTechStacksAtom = atom<string[]>([]);
 
 export const setProjects = (projects: T_project[]) => {
-  projectsAtom.set(projects);
-  processedProjectsAtom.set(projects);
-  updateAvailableTechStacks();
+	projectsAtom.set(projects);
+	processedProjectsAtom.set(projects);
+	updateAvailableTechStacks();
 };
 export const getProjects = (): T_project[] => {
-  return projectsAtom.get();
+	return projectsAtom.get();
 };
 export const getProcessedProjects = (): T_project[] => {
-  return processedProjectsAtom.get();
+	return processedProjectsAtom.get();
 };
 export const getProject = (id: string): T_project | null => {
-  return projectsAtom.get().find((project) => project._id === id) || null;
+	return projectsAtom.get().find((project) => project._id === id) || null;
 };
 
 const sortProjects = (
-  projects: T_project[],
-  sortBy: string,
-  sortOrder: T_sortOrder,
+	projects: T_project[],
+	sortBy: string,
+	sortOrder: T_sortOrder,
 ): void => {
-  switch (sortOrder) {
-    case "asc":
-      if (sortBy === "name") {
-        projects?.sort((a, b) => (a.name > b.name ? 1 : -1));
-      } else if (sortBy === "createdAt") {
-        projects?.sort((a, b) => (a.createdAt > b.createdAt ? 1 : -1));
-      } else if (sortBy === "modifiedAt") {
-        projects?.sort((a, b) => (a.modifiedAt > b.modifiedAt ? 1 : -1));
-      }
-      break;
-    case "desc":
-      if (sortBy === "name") {
-        projects?.sort((a, b) => (a.name < b.name ? 1 : -1));
-      } else if (sortBy === "createdAt") {
-        projects?.sort((a, b) => (a.createdAt < b.createdAt ? 1 : -1));
-      } else if (sortBy === "modifiedAt") {
-        projects?.sort((a, b) => (a.modifiedAt < b.modifiedAt ? 1 : -1));
-      }
-      break;
-  }
-  processedProjectsAtom.set(projects);
+	switch (sortOrder) {
+		case "asc":
+			if (sortBy === "name") {
+				projects?.sort((a, b) => (a.name > b.name ? 1 : -1));
+			} else if (sortBy === "created_at") {
+				projects?.sort((a, b) => (a.created_at > b.created_at ? 1 : -1));
+			} else if (sortBy === "modifiedAt") {
+				projects?.sort((a, b) => (a.modified_at > b.modified_at ? 1 : -1));
+			}
+			break;
+		case "desc":
+			if (sortBy === "name") {
+				projects?.sort((a, b) => (a.name < b.name ? 1 : -1));
+			} else if (sortBy === "createdAt") {
+				projects?.sort((a, b) => (a.created_at < b.created_at ? 1 : -1));
+			} else if (sortBy === "modifiedAt") {
+				projects?.sort((a, b) => (a.modified_at < b.modified_at ? 1 : -1));
+			}
+			break;
+	}
+	processedProjectsAtom.set(projects);
 };
 
 const filterByQueryStrProjects = (projects: T_project[]): void => {
-  const queryStr = searchQueryAtom.get().queryStr;
-  if (queryStr) {
-    const filteredProjects = projects.filter((project) =>
-      project.name.toLowerCase().includes(queryStr.toLowerCase()),
-    );
-    processedProjectsAtom.set(filteredProjects);
-  } else {
-    processedProjectsAtom.set(projects);
-  }
+	const queryStr = searchQueryAtom.get().queryStr;
+	if (queryStr) {
+		const filteredProjects = projects.filter((project) =>
+			project.name.toLowerCase().includes(queryStr.toLowerCase()),
+		);
+		processedProjectsAtom.set(filteredProjects);
+	} else {
+		processedProjectsAtom.set(projects);
+	}
 };
 
 const filterProjects = (projects: T_project[]): void => {
-  const filters = searchQueryAtom.get().filters;
-  const filteredProjects = projects.filter((project) => {
-    if (filters.techStacks) {
-      return filters.techStacks.every((techStack) =>
-        project.techStacks?.includes(techStack),
-      );
-    }
-    if (filters.status) {
-      return project.status === filters.status;
-    }
-    return true;
-  });
-  processedProjectsAtom.set(filteredProjects);
+	const filters = searchQueryAtom.get().filters;
+	const filteredProjects = projects.filter((project) => {
+		if (filters.tech_stacks) {
+			return filters.tech_stacks.every((techStack) =>
+				project.tech_stacks?.includes(techStack),
+			);
+		}
+		if (filters.status) {
+			return project.status === filters.status;
+		}
+		return true;
+	});
+	processedProjectsAtom.set(filteredProjects);
 };
 
 export const processProjects = (): void => {
-  const projects = projectsAtom.get();
-  filterByQueryStrProjects(projects);
-  filterProjects(processedProjectsAtom.get());
-  sortProjects(
-    processedProjectsAtom.get(),
-    searchQueryAtom.get().sortBy,
-    searchQueryAtom.get().sortOrder,
-  );
+	const projects = projectsAtom.get();
+	filterByQueryStrProjects(projects);
+	filterProjects(processedProjectsAtom.get());
+	sortProjects(
+		processedProjectsAtom.get(),
+		searchQueryAtom.get().sort_by,
+		searchQueryAtom.get().sort_order,
+	);
 };
 
 export const updateAvailableTechStacks = (): void => {
-  const processedProjects = processedProjectsAtom.get();
-  const availableTechStacks: string[] = [];
+	const processedProjects = processedProjectsAtom.get();
+	const availableTechStacks: string[] = [];
 
-  processedProjects
-    .flatMap((project) => project.techStacks || [])
-    .map(
-      (techStack: string) =>
-        !availableTechStacks.includes(techStack.trim()) &&
-        availableTechStacks.push(techStack.trim()),
-    ),
-    availableTechStacksAtom.set(availableTechStacks);
+	processedProjects
+		.flatMap((project) => project.tech_stacks || [])
+		.map(
+			(techStack: string) =>
+				!availableTechStacks.includes(techStack.trim()) &&
+				availableTechStacks.push(techStack.trim()),
+		),
+		availableTechStacksAtom.set(availableTechStacks);
 };
